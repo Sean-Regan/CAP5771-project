c97cbcbf22cd0eb1d072b90c36bee7e4e8e12b73
Sean Regan <33043278+Sean-Regan@users.noreply.github.com>
2026-04-19 18:06:16 -0400

    Sql connection and basic filter form

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
+import sqlite3
+
+# Connect to the database
+conn = sqlite3.connect("./data/nutrition.db")
+cur = conn.cursor()
 
 app = Flask(__name__)
 CORS(app)
