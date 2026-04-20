53d87947bb8371705ffd4b237b549816169b1272
dstet <me@dstet.me>
2026-04-20 18:02:59 -0400

    Fix map loading

--- static/map.js
+++ static/map.js
@@ -1,36 +1,57 @@
-// sets the access token, associating the map with your Mapbox account and its permissions
-mapboxgl.accessToken = MAPBOX_API_KEY;
+const CLUSTER_LINK_TEXT = "#cluster";
 
-// creates the map, setting the container to the id of the div you added in step 2, and setting the initial center and zoom level of the map
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
+let MapLoaded = false;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const tabEl = document.querySelector('a[data-bs-toggle="tab"]');
+    
+    if (document.querySelector("#cluster").classList.contains("active")) {
+        loadMap();
+    }
+
+    tabEl.addEventListener('shown.bs.tab', event => {
+        if (event.target instanceof HTMLAnchorElement) {
+            if (event.target.href.includes(CLUSTER_LINK_TEXT) && !MapLoaded) {
+                loadMap();
+            }
+        }
+    })
 });
 
-map.on('load', () => {
-    map.addSource('clusters-source', {
-        'type': 'geojson',
-        'data': `${ENDPOINT}/api/get-clusters.geojson`
+const loadMap = () => {
+    mapboxgl.accessToken = MAPBOX_API_KEY;
+    MapLoaded = true;
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
     });
 
-    map.addLayer({
-        'id': 'circle-layer',
-        'type': 'circle',
-        'source': 'clusters-source',
-        'paint': {
-            'circle-radius': 6,
-            'circle-color': [
-                'interpolate',
-                ['linear'],
-                ['get', 'cluster'],
-                0, '#f80404',
-                30, '#46c2f0',
+    map.on('load', () => {
+        map.addSource('clusters-source', {
+            'type': 'geojson',
+            'data': `${ENDPOINT}/api/get-clusters.geojson`
+        });
 
-            ],
-            // 'circle-color': '#46c2f0',
-            'circle-stroke-width': 2,
-            'circle-stroke-color': 'white'
-        }
-    });
-})
+        map.addLayer({
+            'id': 'circle-layer',
+            'type': 'circle',
+            'source': 'clusters-source',
+            'paint': {
+                'circle-radius': 6,
+                'circle-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'cluster'],
+                    0, '#f80404',
+                    30, '#46c2f0',
+
+                ],
+                // 'circle-color': '#46c2f0',
+                'circle-stroke-width': 2,
+                'circle-stroke-color': 'white'
+            }
+        });
+    })
+};
