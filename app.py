b4ff4e70e9a6e1b26da2b685bf6377fa6e7f7e37
Sean Regan <33043278+Sean-Regan@users.noreply.github.com>
2026-04-20 17:27:07 -0400

    Updated with tabs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import sqlite3
 import os
+import re
 import model_interface
 from dotenv import load_dotenv
 
@@ -18,6 +19,14 @@ cur = conn.cursor()
 app = Flask(__name__)
 CORS(app)
 
+def normalize_text(str):
+    if pd.isna(str):
+        return ""
+    str = str.lower()
+    str = re.sub(r'[^a-z0-9\s]', ' ', str)
+    str = re.sub(r'\s+', ' ', str).strip()
+    return str
+
 @app.route('/')
 def home():
     return render_template('index.html', MAPBOX_API_KEY=MAPBOX_API_KEY, ENDPOINT=ENDPOINT)
@@ -28,20 +37,25 @@ def api_get_clusters():
 
 @app.route('/api/nutrition')
 def api_get_nutrition_info():
+    conn.create_function('normalize_text', 1, normalize_text)
+
     search_name = request.args.get('name')
     search_brand = request.args.get('brand')
+    search_nutrient = request.args.get('nutrient')
 
     wal_df = pd.read_sql_query("""
-                       SELECT price_retail
-                       FROM walmart_price
-                       WHERE product_name LIKE ? AND brand LIKE ?
-                       """, conn, params=(f"%{search_name.value}%", f"%{search_brand.value}%"))
+                       SELECT clean_desc, name, MIN(amount) as amount, MIN(price_retail) as price
+                       FROM wal_nutrient
+                       WHERE product_name LIKE ? AND clean_brand LIKE ? AND normalize_text(name) LIKE ?
+                       GROUP BY clean_desc, name
+                       """, conn, params=(f"%{search_name.value}%", f"%{normalize_text(search_brand.value)}%", f"%{normalize_text(search_nutrient.value)}%"))
     
     wf_df = pd.read_sql_query("""
-                       SELECT price
-                       FROM wholefoods_price
-                       WHERE product_name LIKE ? AND brand LIKE ?
-                       """, conn, params=(f"%{search_name.value}%", f"%{search_brand.value}%"))
+                       SELECT clean_desc, name, MIN(amount) as amount, MIN(price) as price
+                       FROM wf_nutrient
+                       WHERE product_name LIKE ? AND clean_brand LIKE ? AND normalize_text(name) LIKE ?
+                       GROUP BY clean_desc, name
+                       """, conn, params=(f"%{search_name.value}%", f"%{normalize_text(search_brand.value)}%", f"%{normalize_text(search_nutrient.value)}%"))
 
 if __name__ == '__main__':
     model_interface.init_clusters(conn, cur)
